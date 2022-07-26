3493b7d6fd37d53ac9990b0ded50f564e0ad0b93
minh le <minh.le@construct.sg>
2022-07-26 18:24:30 +0700

    update base header

--- components/BaseHeader/index.jsx
+++ components/BaseHeader/index.jsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import NUSLogo from '../../public/images/nus-logo.png';
 
 const BaseHeader = () => {
   return (
-    <header>
-      <div className='md:px-10'>
-        <div className='py-9 px-7 md:px-0'>
-          <a href='#/'>
+    <header className='md:px-10'>
+      <div className='py-9 px-7 md:px-0'>
+        <Link href='/'>
+          <a>
             <Image src={NUSLogo} alt='logo' priority />
           </a>
-        </div>
+        </Link>
       </div>
     </header>
   );
