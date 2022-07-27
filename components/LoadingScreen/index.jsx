b787dbf44826aceec1b81b611086e29806824c28
MinhLe <minh.le@construct.sg>
2022-07-27 10:37:28 +0700

    update ant design

--- components/LoadingScreen/index.jsx
+++ components/LoadingScreen/index.jsx
@@ -1,9 +1,10 @@
+import { Spin } from 'antd';
+
 const LoadingScreen = () => {
   return (
     <div className='fixed w-full h-full flex flex-col items-center justify-center top-0 left-0'>
       {/* <img className="mb-5" src={`${assetsURL}/nus-logo.png`} alt="logo" /> */}
-      {/* <Spin size="large" /> */}
-      Loading...
+      <Spin size='large' />
     </div>
   );
 };
