bbf756393de887510508404b789a22e6c376966b
minh le <minh.le@construct.sg>
2022-07-31 21:16:53 +0700

    update framer motion for jid page

--- pages/journey/[jid].jsx
+++ pages/journey/[jid].jsx
@@ -1,4 +1,5 @@
 import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import { getJourney, getJourneyDetail } from '../../api';
 import BackIcon from '../../components/BackIcon';
@@ -39,70 +40,86 @@ const JourneyDetail = ({ journeyDetails, journeys }) => {
     }, 100);
   };
 
+  const variants = {
+    hidden: {
+      scale: 0.8,
+      opacity: 0,
+    },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        delay: 0.4,
+      },
+    },
+  };
+
   return (
-    <section className='relative'>
-      {/* grid md:grid-cols-3 gap-5  */}
-      <div className='space-y-5 px-7 mb-20'>
-        <figure
-          ref={floatingNav}
-          className={`${floatingNavClasses}`}
-          onClick={handleBack}
-        >
-          <div className='flex items-end cursor-pointer space-x-4 max-w-1456 mx-auto md:px-7'>
+    <motion.div initial='hidden' animate='visible' variants={variants}>
+      <section className='relative'>
+        {/* grid md:grid-cols-3 gap-5  */}
+        <div className='space-y-5 px-7 mb-20'>
+          <figure
+            ref={floatingNav}
+            className={`${floatingNavClasses}`}
+            onClick={handleBack}
+          >
+            <div className='flex items-end cursor-pointer space-x-4 max-w-1456 mx-auto md:px-7'>
+              <BackIcon />
+              <span className='text-xl'>View all journeys</span>
+            </div>
+          </figure>
+
+          <figure
+            className='flex items-end cursor-pointer space-x-4'
+            onClick={handleBack}
+          >
             <BackIcon />
             <span className='text-xl'>View all journeys</span>
-          </div>
-        </figure>
+          </figure>
 
-        <figure
-          className='flex items-end cursor-pointer space-x-4'
-          onClick={handleBack}
-        >
-          <BackIcon />
-          <span className='text-xl'>View all journeys</span>
-        </figure>
-
-        <SwitchPersona handleOnClick={switchPersona} jID={jid} layout={''} />
-      </div>
-
-      <div className='md:px-7 space-y-10'>
-        <div className='text-lg md:text-2xl font-bold text-center px-10'>
-          Click the boxes below to browse your selected journey.
+          <SwitchPersona handleOnClick={switchPersona} jID={jid} layout={''} />
         </div>
 
-        <div className='space-y-7 px-7 py-9 md:rounded-xl bg-nus-gray-200'>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7'>
-            {journeyDetail.data.general.sections?.map((item, index) => (
-              <figure
-                key={index}
-                className={`rounded-xl bg-white p-4 min-w-220 md:min-w-0 shadow-lg`}
-              >
-                <div className='flex justify-between space-x-2 h-full'>
-                  <figure className='flex-1'>
-                    <span
-                      className={`inline-block text-base md:text-lg leading-snug font-bold text-nus-black-200 mb-2`}
-                    >
-                      {item.title}
-                    </span>
+        <div className='md:px-7 space-y-10'>
+          <div className='text-lg md:text-2xl font-bold text-center px-10'>
+            Click the boxes below to browse your selected journey.
+          </div>
 
-                    <div
-                      className='text-sm md:text-base'
-                      dangerouslySetInnerHTML={{ __html: item.desc }}
+          <div className='space-y-7 px-7 py-9 md:rounded-xl bg-nus-gray-200'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7'>
+              {journeyDetail.data.general.sections?.map((item, index) => (
+                <figure
+                  key={index}
+                  className={`rounded-xl bg-white p-4 min-w-220 md:min-w-0 shadow-lg`}
+                >
+                  <div className='flex justify-between space-x-2 h-full'>
+                    <figure className='flex-1'>
+                      <span
+                        className={`inline-block text-base md:text-lg leading-snug font-bold text-nus-black-200 mb-2`}
+                      >
+                        {item.title}
+                      </span>
+
+                      <div
+                        className='text-sm md:text-base'
+                        dangerouslySetInnerHTML={{ __html: item.desc }}
+                      />
+                    </figure>
+
+                    <BtnArrow
+                      onClick={() => handleRecommendedDetail(index, item)}
+                      className='self-end'
+                      theme='white'
                     />
-                  </figure>
-
-                  <BtnArrow
-                    onClick={() => handleRecommendedDetail(index, item)}
-                    className='self-end'
-                    theme='white'
-                  />
-                </div>
-              </figure>
-            ))}
+                  </div>
+                </figure>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </motion.div>
   );
 };
 
